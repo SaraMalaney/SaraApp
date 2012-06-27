8f11ee5fdc4fe7b958d5fba152527079bc10bfe7
Sarah <sksellio@gmail.com>
2012-06-27 17:44:30 -0400

    Some spacing updates.

--- src/com/example/saraapp/SaraAppActivity.java
+++ src/com/example/saraapp/SaraAppActivity.java
@@ -21,32 +21,31 @@ public class SaraAppActivity extends Activity {
       
         ViewGroup vg = (ViewGroup)findViewById(R.id.vg);
         vg.setOnTouchListener(new View.OnTouchListener() {
-        	@Override
+            @Override
             public boolean onTouch(View v, MotionEvent event) {
-                    switch(event.getActionMasked())
-                    {
-                            case MotionEvent.ACTION_MOVE:
-                                    int x = (int)event.getX() - offset_x;
-                                    int y = (int)event.getY() - offset_y;
+                switch(event.getActionMasked()) {
+                    case MotionEvent.ACTION_MOVE:
+                        int x = (int)event.getX() - offset_x;
+                        int y = (int)event.getY() - offset_y;
 
-            int w = getWindowManager().getDefaultDisplay().getWidth() - 100;
-            int h = getWindowManager().getDefaultDisplay().getHeight() - 100;
-            if(x > w)
-                x = w;
-            if(y > h)
-                y = h;
-                             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                            new ViewGroup.MarginLayoutParams(
-                                            LinearLayout.LayoutParams.WRAP_CONTENT,
-                                            LinearLayout.LayoutParams.WRAP_CONTENT));
-                             lp.setMargins(x, y, 0, 0);
+                        int w = getWindowManager().getDefaultDisplay().getWidth() - 100;
+                        int h = getWindowManager().getDefaultDisplay().getHeight() - 100;
+                        if(x > w)
+                            x = w;
+                        if(y > h)
+                            y = h;
+                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        new ViewGroup.MarginLayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT));
+                                lp.setMargins(x, y, 0, 0);
 
-                                    selected_item.setLayoutParams(lp);
-                                    break;
-                            default:
-                                    break;
-                    }
-                    return true;
+                        selected_item.setLayoutParams(lp);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
             }
 });
 ImageView img = (ImageView)findViewById(R.id.img);
